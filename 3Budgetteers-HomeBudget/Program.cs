4c93dc2f1db09536d89040b5f734215b0de8988b
breannadefo <98484910+breannadefo@users.noreply.github.com>
2023-02-09 09:28:50 -0500

    Add project files.

---
+++ 3Budgetteers-HomeBudget/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _3Budgetteers_HomeBudget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
