83cbfbf4b8c59bd6e3386816d8b5cc3fbd82ad7a
breannadefo <98484910+breannadefo@users.noreply.github.com>
2023-02-16 09:06:01 -0500

    Created the method that closes a connection to a database.

---
+++ 3Budgetteers-HomeBudget/Database.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget
+{
+    public class Database
+    {
+        //backing fields
+        private static SQLiteConnection _connection;
+
+        //properties
+        public static SQLiteConnection dbConnection
+        {
+            get { return _connection; }
+        }
+
+        //member methods
+
+        public static void CloseDatabaseAndReleaseFile()
+        {
+            if (dbConnection != null)
+            {
+                //closing the connection to the database
+                dbConnection.Close();
+
+                //calls the garbage collector to make sure the connection is ended
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+        }
+
+    }
+}
