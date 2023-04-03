571d4b4d79ff92adcc03bf2252bb9db68eda4ba4
breannadefo <2145494@johnabbottcollege.net>
2023-04-03 12:53:35 -0400

    Added basic UI and initialized the comboBox.

---
+++ HomeBudgetWPF/AddCategory.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+using Budget;
+
+namespace HomeBudgetWPF
+{
+    /// <summary>
+    /// Interaction logic for AddCategory.xaml
+    /// </summary>
+    public partial class AddCategory : Window
+    {
+        public AddCategory()
+        {
+            InitializeComponent();
+            InitializeComboBox();
+        }
+
+        private void InitializeComboBox()
+        {
+            cmb_types.ItemsSource = Enum.GetValues(typeof(Category.CategoryType));
+            cmb_types.SelectedItem = Category.CategoryType.Expense;
+        }
+    }
+}
