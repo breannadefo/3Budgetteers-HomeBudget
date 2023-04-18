a96e51cb59c9a5a93a7983ee4731bce1127eadce
breannadefo <98484910+breannadefo@users.noreply.github.com>
2023-04-18 12:08:50 -0400

    Added navigation buttons to the display expenses page.

---
+++ HomeBudgetWPF/DisplayExpenses.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HomeBudgetWPF
+{
+    /// <summary>
+    /// Interaction logic for DisplayExpenses.xaml
+    /// </summary>
+    public partial class DisplayExpenses : Window
+    {
+        public DisplayExpenses()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_AddExpense_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_AddCategory_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
