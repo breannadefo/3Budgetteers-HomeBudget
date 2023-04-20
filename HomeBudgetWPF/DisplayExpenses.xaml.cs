40074f90b84d75ca276e8a183e142240bf96a8be
breannadefo <98484910+breannadefo@users.noreply.github.com>
2023-04-20 11:08:23 -0400

    Added more styling to the display expenses window.

--- HomeBudgetWPF/DisplayExpenses.xaml.cs
+++ HomeBudgetWPF/DisplayExpenses.xaml.cs
@@ -23,7 +23,7 @@ namespace HomeBudgetWPF
         public DisplayExpenses()
         {
             InitializeComponent();
-            InitializeComboBox();
+            //InitializeComboBox();
         }
 
         private void InitializeComboBox()
