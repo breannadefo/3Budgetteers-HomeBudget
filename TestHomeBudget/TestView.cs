44c4507309811d0d03ecd37d8bfddb3df3d9abdd
Breanna <bdeforest01@gmail.com>
2023-04-26 02:51:48 -0400

    Added the new view methods to the test interface. (Has a problem though because it doesn't recognize the assebly reference?)

--- TestHomeBudget/TestView.cs
+++ TestHomeBudget/TestView.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Budget;
 using HomeBudgetWPF;
 
 
@@ -13,13 +15,23 @@ namespace TestHomeBudget
         bool _calledResetValues;
         bool _calledShowErrorMessages;
         bool _calledShowSuccessMessages;
+        bool _calledDisplayExpenses;
+        bool _calledDisplayByMonth;
+        bool _calledDisplayByCategory;
+        bool _calledDisplayByMonthAndCategory;
 
         public TestView()
         {
             _calledResetValues = false;
             _calledShowErrorMessages = false;
             _calledShowSuccessMessages = false;
+            _calledDisplayExpenses = false;
+            _calledDisplayByMonth = false;
+            _calledDisplayByCategory = false;
+            _calledDisplayByMonthAndCategory = false;
         }
+
+        #region properties
 
         public bool CalledResetValues
         {
@@ -36,11 +48,59 @@ namespace TestHomeBudget
             get { return _calledShowSuccessMessages; }
         }
 
+        public bool CalledDisplayExpenses
+        {
+            get { return _calledDisplayExpenses; }
+        }
+
+        public bool CalledDisplayByMonth
+        {
+            get { return _calledDisplayByMonth; }
+        }
+
+        public bool CalledDisplayByCategory
+        {
+            get { return _calledDisplayByCategory; }
+        }
+
+        public bool CalledDisplayByMonthAndCategory
+        {
+            get { return _calledDisplayByMonthAndCategory; }
+        }
+
+        #endregion
+
+        #region methods
+
         public void MakeAllValuesFalse()
         {
             _calledResetValues = false;
             _calledShowErrorMessages = false;
             _calledShowSuccessMessages = false;
+            _calledDisplayExpenses = false;
+            _calledDisplayByMonth = false;
+            _calledDisplayByCategory = false;
+            _calledDisplayByMonthAndCategory = false;
+        }
+
+        public void DisplayExpensesByCategoryInGrid(List<BudgetItemsByCategory> items)
+        {
+            _calledDisplayByCategory = true;
+        }
+
+        public void DisplayExpensesByMonthInGrid(List<BudgetItemsByMonth> items)
+        {
+            _calledDisplayByMonth = true;
+        }
+
+        public void DisplayExpensesInGrid(List<BudgetItem> items)
+        {
+            _calledDisplayExpenses = true;
+        }
+
+        public void DisplayExpensesInGridDictionary(List<Dictionary<string, object>> items)
+        {
+            _calledDisplayByMonthAndCategory = true;
         }
 
         public void ResetValues()
@@ -57,5 +117,7 @@ namespace TestHomeBudget
         {
             _calledShowSuccessMessages = true;
         }
+
+        #endregion
     }
 }
